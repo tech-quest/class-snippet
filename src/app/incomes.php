2411d289432396c3abcc8f2048e2b6306746a932
KatsuyaTanaka95 <gu0520kt@gmail.com>
2022-08-09 08:54:17 +0900

    classの作成

---
+++ src/app/incomes.php
@@ -0,0 +1,27 @@
+<?php
+
+class Imcomes
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $dbUserName = 'root';
+        $dbPassword = 'password';
+        $pdo = new PDO(
+            'mysql:host=mysql; dbname=tq_quest; charset=utf8',
+            $dbUserName,
+            $dbPassword
+        );
+        $this->pdo = $pdo;
+    }
+
+    public function findAll()
+    {
+        $sql = 'SELECT * FROM incomes';
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute();
+        $incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $incomes;
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Welcome TECH QUEST!';
+require_once __DIR__ . '../../app/incomes.php';
+
+$incomesTable = new Imcomes();
+$incomes = $incomesTable->findAll();
+
+echo '<pre>';
+var_dump($incomes);
+echo '<pre>';
